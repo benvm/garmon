b7c51fb125c3162125f155476e2120b94124ce16
Ben Van Mechelen <me@benvm.be>
2011-03-09 03:19:31 +0100

    distutils package created in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,17 @@ from distutils.core import setup
 
 import garmon
 
+setup ( name='garmon',
+        version=garmon.version,
+        description='GTK OBD-II Scantool',
+        author='Ben Van Mechelen',
+        author_email='me@benvm.be',
+        url='http://github.com/benvm/garmon',
+        packages=['garmon',],
+        package_data={'garmon': ['data/*']},
+        scripts=['scripts/garmon',],
+        license=garmon.license,
+        data_files=[('share/applications', ['garmon.desktop',]),],
+      )
 
 
